860813f3066e5f55b44ac79c41fdf13dbff25f81
turnin19 <turnin19@earthlink.net>
2020-09-12 14:29:07 -0400

    Starting pirple archive.

---
+++ pirple/oopcsharp/VS/Lessons/01HelloWorld/01HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("My first Pirple program.");
+            Console.WriteLine("NEW EDIT!!!");
+
+            Console.ReadLine(); // To keep Run() window visible until I press ENTER.
+        }
+    }
+}
