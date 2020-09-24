cc1480fccd1e5da68bc850e88b4bc6ac010c0404
turnin19 <turnin19@earthlink.net>
2020-09-24 14:48:11 -0400

    Starting new python course, a couple JS projects, css flex.

---
+++ codingphase/jsfutureproof/VS/01_Intro_Numbers/Activity01/Activity01/script.js
@@ -0,0 +1,29 @@
+﻿var box = "box";
+console.log(box);
+box = 6;
+console.log(box);
+var car = "honda";
+console.log(car);
+
+var apples = 5;
+console.log(apples * 20);
+
+var a = 20;
+var b = 10;
+var c = a + b;
+console.log(c);
+
+var Car = "Chevrolet";
+console.log(car);
+console.log(Car);
+
+var cAr = undefined;
+console.log(cAr);
+
+// Snake case is variable separted by underline.
+var snake_type = "Viper";
+console.log(snake_type);
+
+// Camel case first letter of first word lc, all subsequent words first letter uc.
+var camelCase = "Python";
+console.log(camelCase);
