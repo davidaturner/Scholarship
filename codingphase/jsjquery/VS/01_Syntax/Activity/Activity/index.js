0fafaf9ebe7525e6ad4a625ea7bd194131a92c42
turnin19 <turnin19@earthlink.net>
2020-09-24 20:43:28 -0400

    Worked on JQuery exercises.

---
+++ codingphase/jsjquery/VS/01_Syntax/Activity/Activity/index.js
@@ -0,0 +1,16 @@
+﻿jQuery('body').css({ backgroundColor: "purple" });
+
+//$('body').css({ backgroundColor: 'purple' });
+
+jQuery('div').css({ padding: '10px' });
+
+jQuery('.box1').css({ backgroundColor: "orange", width: "100px", height: "100px" });
+
+jQuery('.box2').css({ backgroundColor: "yellow" });
+
+jQuery('p').css({ backgroundColor: "green" });
+
+//check out api.jQuery.com
+
+jQuery('#last p').css({ backgroundColor: "gray" }).fadeOut('slow', function () {
+});
