fca3d388a4bae360b2216030d3122748e38a24e5
turnin19 <turnin19@earthlink.net>
2023-09-30 14:51:34 -0400

    cp angular more

--- codingphase/js/angular/VSC/08_P2_Movies/movie-app/src/app/app.module.ts
+++ codingphase/js/angular/VSC/08_P2_Movies/movie-app/src/app/app.module.ts
@@ -8,6 +8,11 @@ import { CheckoutComponent } from './layouts/checkout/checkout.component';
 import { MovieComponent } from './layouts/movie/movie.component';
 import { ScheduleComponent } from './layouts/schedule/schedule.component';
 import { ThankyouComponent } from './layouts/thankyou/thankyou.component';
+import { HeaderComponent } from './layouts/shared/header/header.component';
+import { FooterComponent } from './layouts/shared/footer/footer.component';
+import { PageExampleComponent } from './layouts/shared/page-example/page-example.component';
+import { PageHeaderComponent } from './layouts/shared/page-header/page-header.component';
+import { MainLayoutComponent } from './layouts/shared/main-layout/main-layout.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +21,12 @@ import { ThankyouComponent } from './layouts/thankyou/thankyou.component';
     CheckoutComponent,
     MovieComponent,
     ScheduleComponent,
-    ThankyouComponent
+    ThankyouComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageExampleComponent,
+    PageHeaderComponent,
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
---
+++ codingphase/js/angular/VSC/08_P2_Movies/movie-app/src/app/layouts/shared/page-example/page-example.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-example',
+  templateUrl: './page-example.component.html',
+  styleUrls: ['./page-example.component.css']
+})
+export class PageExampleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
