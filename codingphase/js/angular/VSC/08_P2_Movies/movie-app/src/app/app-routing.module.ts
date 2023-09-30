ce3e94d0a6ce255dea029a0f4e24388fda1af12e
turnin19 <turnin19@earthlink.net>
2023-09-30 08:23:46 -0400

    cp angular project

--- codingphase/js/angular/VSC/08_P2_Movies/movie-app/src/app/app-routing.module.ts
+++ codingphase/js/angular/VSC/08_P2_Movies/movie-app/src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppHomeComponent } from './layouts/app-home/app-home.component';
+import { MovieComponent } from './layouts/movie/movie.component';
+import { ScheduleComponent } from './layouts/schedule/schedule.component';
+import { CheckoutComponent } from './layouts/checkout/checkout.component';
+import { ThankyouComponent } from './layouts/thankyou/thankyou.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppHomeComponent
+  },
+  {
+    path: 'movie',
+    component: MovieComponent
+  },
+  {
+    path: 'movie/:title',
+    component: MovieComponent
+  },
+  {
+    path: 'movie/:title/schedule',
+    component: ScheduleComponent
+  },
+  {
+    path: 'movie/:title/schedule/checkout',
+    component: CheckoutComponent
+  },
+  {
+    path: 'movie/:title/schedule/checkout/thank-you',
+    component: ThankyouComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
